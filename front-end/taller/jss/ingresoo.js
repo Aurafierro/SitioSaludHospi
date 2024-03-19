14d6907f0264b364e478c11f148ff969bf97d204
Aurafierro <auramariafierrofierro@gmail.com>
2024-03-19 07:55:41 -0500

    Correciones en el error de spring y en js

--- front-end/taller/jss/ingresoo.js
+++ front-end/taller/jss/ingresoo.js
@@ -182,7 +182,7 @@ function registrarIngreso() {
       //validacion para que no hayan varios ingresos con un mismo paciente
       error: function (error) {
         // alert("error al guardar".error);
-         Swal.fire("Error", "Error al guardar "+error.responseText, "error");
+         Swal.fire("Error", "Error al guardar, "+error.responseText, "error");
        },
     })
   } else {
